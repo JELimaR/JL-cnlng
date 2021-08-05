a31c034516c8217f28bedd0fff127a9e899b5606
JELimaR <juanlimarod@gmail.com>
2021-08-05 12:05:20 -0300

    firsts steps

--- src/test/LangController.test.ts
+++ src/test/LangController.test.ts
@@ -5,5 +5,21 @@ let per: string = '';
 
 test( 'new instance', () => {
     let lcother = Langcontroller.instance
-    expect( lcother === lc ).toBeTruthy()
+    expect( lcother === lc ).toBeTruthy();
+    expect( lc.subFamArray.length ).toEqual(0);
+    expect( lc.selectedTree ).toBeUndefined();
+    expect( lc.getSelected() ).toBeUndefined();
+    lc.reset();
+})
+
+test('addFam & get subFamArray withoot selected', () => {
+    lc.createFam();
+    expect( lc.subFamArray.length ).toEqual(0);
+    lc.addFam();
+    expect( lc.subFamArray.length ).toEqual(1);
+    for (let i = 0; i<10;i++) {
+        lc.createFam();
+        lc.addFam();
+    }
+    expect( lc.subFamArray.length ).toEqual(11);
 })
